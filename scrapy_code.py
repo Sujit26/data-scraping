ee033400b3e71cb529f0161ca6285998e28acd82
Sujit26 <45810709+Sujit26@users.noreply.github.com>
2021-01-11 13:06:22 +0530

    Create scrapy_code.py

---
+++ scrapy_code.py
@@ -0,0 +1,43 @@
+import scrapy
+from scrapy.http import HtmlResponse
+
+class BrickSetSpider(scrapy.Spider):
+    name = 'medium'
+    base_url = 'https://medium.com/tag/'
+
+    def start_requests(self):
+        search_query = 'flutter'
+        # search_query = 'data-science'
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36'
+        }
+        yield scrapy.http.Request(
+            self.base_url+search_query,
+            headers=headers,
+            callback=self.parse
+        )
+
+    def parse(self, response: HtmlResponse):
+        SET_SELECTOR = '.streamItem'
+        for article in response.css(SET_SELECTOR):
+            data = article.css('a ::text').extract()
+            read_time = article.css(
+                'span.readingTime ::attr(title)').extract_first()
+
+            blog_link = article.css("div.postArticle-readMore a::attr(href)")[-1]
+
+
+            print("\n"*5)
+            nor = data[-1]
+            # if nor.lower()._contains_('read'):
+            #     nor = '0 responses'
+            formatted_data = {
+                'name': data[0],
+                'date': data[2],
+                'title': data[3],
+                'short_desciption': data[4],
+                'responses': nor,
+                'read_time': read_time,
+                "blog_link": blog_link
+            }
+            yield formatted_data
